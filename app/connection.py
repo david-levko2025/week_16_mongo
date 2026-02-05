fbafb9c516b295902225786a371cb8a2d44319ea
david-levko2025 <mdavidl2003@gmail.com>
2026-02-05 13:38:16 +0200

    Change default MongoDB URI from 'fastapi' to 'mongo'

--- app/connection.py
+++ app/connection.py
@@ -2,7 +2,7 @@ import json
 from pymongo import MongoClient
 from os import getenv
 
-mongo_uri = getenv("MONGO_URI", "fastapi")
+mongo_uri = getenv("MONGO_URI", "mongo")
 mongo_db = getenv("MONGO_DB", "testdb")
 mongo_collection = getenv("MONGO_COLLECTION", "testcollection")
 file_path = './employee.json'
